d30bbe990de94704b68389bc7fed9f5a83fe1029
ram <ramc20986@gmail.com>
2024-07-09 19:42:30 +0530

    implement client, server delivery and connection state recovery

--- index.js
+++ index.js
@@ -12,52 +12,57 @@ async function main() {
     });
 
     await db.exec(`
-        CREATE TABLE IF NOT EXISTS messages (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            client_offset TEXT UNIQUE,
-            content TEXT
-        );
-      `);
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {
+            maxDisconnectionDuration: 2 * 60 * 1000,
+            skipMiddlewares: true
+        }
+    });
+
+    app.get('/', (req, res) => {
+        res.sendFile(join(__dirname, 'public/index.html'));
+    });
+
+    io.on('connection', async (socket) => {
+        socket.on('chat message', async (msg, clientOffset, callback) => {
+            let result;
+            try {
+                result = await db.run('INSERT INTO messages (content, clientOffset) VALUES (?, ?)', msg, clientOffset);
+            } catch (e) {
+                if (e.errno === 19) {
+                    callback();
+                } else { }
+                return
+            }
+            io.emit('chat message', msg, result.lastID);
+            callback();
+        }); 
+        if (!socket.recovered) {
+            try {
+                await db.each('SELECT id, content FROM messages WHERE id > ?',
+                    [socket.handshake.auth.serverOffset || 0],
+                    (_err, row) => {
+                        socket.emit('chat message', row.content, row.id);
+                    }
+                )
+            } catch (err) {
+                console.log('err in recovered', err)
+            }
+        }
+    });
+
+    server.listen(3000, () => {
+        console.log('server running at port no. 3000');
+    });
 }
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, './public/index.html'))
-})
-
-io.on('connection', async (socket) => {
-    socket.on('chat message', async (msg) => {
-        let result;
-        try {
-            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
-        } catch (error) {
-            return;
-        }
-
-        io.emit('chat message', msg, result.lastID);
-    })
-
-    if (!socket.recovered) {
-        try {
-            await db.each('SELECT id, content FROM messages WHERE id > ?',
-                [socket.handshake.auth.serverOffset || 0],
-                (_err, row) => {
-                    socket.emit('chat message', row.content, row.id);
-                }
-            )
-        } catch (error) {
-            console.log('err in recovered msg', error)
-        }
-    }
-})
-
-server.listen(3000, () => {
-    console.log('server listen at port no. 3000')
-})
-
-main(); 
+main();
